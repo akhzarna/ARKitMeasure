9dbbfad3678bb71225fe72828c680bf207e279dc
akhzarna <link2akhzar@gmail.com>
2023-04-09 12:52:10 +0500

    Initial Commit

---
+++ Measure/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Measure
+//
+//  Created by Akhzar Nazir on 09/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Measure/MeasureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeasureApp.swift
+//  Measure
+//
+//  Created by Akhzar Nazir on 09/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MeasureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
