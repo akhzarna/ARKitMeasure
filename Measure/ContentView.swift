4d933ed8e5edca11dd417233a8d9b9c297394e03
akhzarna <link2akhzar@gmail.com>
2023-04-09 13:09:40 +0500

    first commit

--- Measure/ContentView.swift
+++ Measure/ContentView.swift
@@ -1,26 +1,33 @@
 //
 //  ContentView.swift
-//  Measure
+//  ARMeasure
 //
-//  Created by Akhzar Nazir on 09/04/2023.
+//  Created by DAISUKEMAJIMA on 2022/02/04.
 //
 
 import SwiftUI
+import SceneKit
 
-struct ContentView: View {
+struct ContentView : View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        return ViewControllerContainer().edgesIgnoringSafeArea(.all)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ViewControllerContainer: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> ViewController {
+        
+        let viewController = ViewController()
+        return viewController
+        
+    }
+    
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {}
+    func makeCoordinator() -> ViewControllerContainer.Coordinator {
+        return Coordinator()
+    }
+    class Coordinator {
+        
     }
 }
